625056a64f15d2bf489deba9e224a8c171e9c196
Andreas Groos <andreas.groos1@gmail.com>
2020-11-16 07:21:15 -0800

    <Test> component, with Sass added

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import logo from './logo.svg';
-import './App.css';
+import './App.scss';
+import Test from './Test'
 
 function App() {
   return (
@@ -17,6 +18,7 @@ function App() {
         >
           Learn React
         </a>
+        <Test message="Hello World" />
       </header>
     </div>
   );
---
+++ src/Test.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function Test({message}) {
+  return (
+    <div id="test">
+      <h1> This is the test component </h1>
+      <p>{message}</p>
+    </div>
+  )
+}
